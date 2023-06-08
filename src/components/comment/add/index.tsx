006514c79dc881a66188c1e66e02e9ba146b2820
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-06-08 15:18:08 +0200

    added input responsive

--- src/components/comment/add/index.tsx
+++ src/components/comment/add/index.tsx
@@ -2,6 +2,7 @@ import { AppButton } from "@/components/app/button";
 import { IUser } from "@/interfaces/user";
 import Image from "next/image";
 import { CommentAddContainer } from "./CommentAddContainer";
+import { CommentAddLayout } from "./CommentAddLayout";
 import { CommentAddTextarea } from "./CommentAddTextarea";
 
 interface IProps {
@@ -11,14 +12,18 @@ interface IProps {
 export const CommentAdd = ({ currentUser }: IProps) => {
   return (
     <CommentAddContainer>
-      <Image
-        src={`/${currentUser.image.png}`}
-        alt={currentUser.username}
-        height={40}
-        width={40}
+      <CommentAddLayout
+        avatar={
+          <Image
+            src={`/${currentUser.image.png}`}
+            alt={currentUser.username}
+            height={40}
+            width={40}
+          />
+        }
+        textarea={<CommentAddTextarea />}
+        button={<AppButton>Send</AppButton>}
       />
-      <CommentAddTextarea />
-      <AppButton>Send</AppButton>
     </CommentAddContainer>
   );
 };
